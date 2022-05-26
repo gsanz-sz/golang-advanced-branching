ef97b7935d3e60391776e59d032238d5bf3836af
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-05-25 22:30:55 -0300

    Restart module 2

--- vehicle.go
+++ vehicle.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 )
 
 type vehicle interface {
@@ -101,38 +100,5 @@ func readJSONFile() Values {
 }
 
 func generateRating() {
-	f := readJSONFile()
 
-	for _, v := range f.Models {
-		for _, msg := range v.Feedback {
-			if text := strings.Split(msg, " "); len(text) >= 15 {
-				var vehResult feedbackResult
-				var vehRating rating
-				vehRating = 5.0
-				vehResult.feedbackTotal++
-				for _, word := range text {
-					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
-					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
-						vehRating += extraPositive
-					case "help", "helpful", "thanks", "thank you", "happy":
-						vehRating += positive
-					case "not helpful", "sad", "angry", "improve", "annoy":
-						vehRating += negative
-					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
-						vehRating += extraNegative
-					}
-
-				}
-				switch {
-				case vehRating > 8.0:
-					vehResult.feedbackPositive++
-				case vehRating >= 4.0 && vehRating <= 8.0:
-					vehResult.feedbackNeutral++
-				case vehRating < 4.0:
-					vehResult.feedbackNegative++
-				}
-				vehicleResult[v.Name] = vehResult
-			}
-		}
-	}
 }
