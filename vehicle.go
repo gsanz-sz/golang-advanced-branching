44441d9d94715fc28e6806f9302f2b0ca2a728c9
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-05-25 23:12:39 -0300

    print model variables and set to true

--- vehicle.go
+++ vehicle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -140,7 +141,8 @@ func showRating(model string) {
 	ratingFound := false
 	for m, r := range vehicleResult {
 		if m == model {
-
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
 		}
 	}
 }
